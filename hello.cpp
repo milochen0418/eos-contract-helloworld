d41a198bd276c60b8bb68efc828d4cdd50f10939
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-06 13:37:53 +0000

    add hiauth function for require_auth()

--- hello.cpp
+++ hello.cpp
@@ -6,12 +6,20 @@ using namespace eosio;
 class hello : public contract {
 	public:
 		using contract::contract;
+		
 		[[eosio::action]]
 		void hi( name user ) {
-			print("Hello, ", user);
+			print("Hi, ", user);
+		}
+
+		[[eosio::action]]
+		void hiauth( name user ) {
+			require_auth( user );
+			print("HiAuth, ", user);
 		}
 };
 
-EOSIO_DISPATCH( hello, (hi))
+//EOSIO_DISPATCH( hello, (hi));
+EOSIO_DISPATCH( hello, (hiauth));
 
 
