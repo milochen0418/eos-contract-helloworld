c3b08b41a694a912d809b417888ff798883543c0
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-06 08:53:18 +0000

    improve build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ CONTRACT_NAME=hello
 
 #eosio-cpp -o hello.wasm hello.cpp --abigen
 eosio-cpp -o $CONTRACT_NAME.wasm hello.cpp --abigen
-mkdir -p $CONTRACT_NAME
-cp $CONTRACT_NAME.wasm ./$CONTRACT_NAME/
-cp $CONTRACT_NAME.abi ./$CONTRACT_NAME/
+rm -rf  ./$CONTRACT_NAME
+mkdir -p ./$CONTRACT_NAME
+mv $CONTRACT_NAME.wasm ./$CONTRACT_NAME/
+mv $CONTRACT_NAME.abi ./$CONTRACT_NAME/
